c94eaf75c1f67d85c47573469f4abfccc45c255a
yongminkim0501 <yongmingim166@gmail.com>
2025-02-16 18:13:27 +0900

    gpt 관련 코드 클래스화

--- ChatGptApiManage.py
+++ ChatGptApiManage.py
@@ -7,21 +7,22 @@ def connect_chatGpt(path):
         TOKEN = config['chatGpt_token']
     return TOKEN
 
-gpt_token = connect_chatGpt("ChatGpt_token.json")
+class gpt_object:
+    def __init__(self,):
+        self.token = connect_chatGpt("ChatGpt_token.json")
+        self.client = OpenAI(
+            api_key = self.token
+        )
 
-client = OpenAI(
-    api_key = gpt_token
-)
-
-completion = client.chat.completions.create(
-    model = "gpt-3.5-turbo",
-    messages=[
-        {
-            "role":"user",
-            "content":"강아지에 대해서 설명해줘"}
-    ]
-)
-
-print(completion.choices[0].message)
+    def send_chatGpt_server(self, message):
+        completion = self.client.chat.completions.create(
+            model = "gpt-3.5-turbo",
+            messages=[
+                {
+                    "role":"user",
+                    "content": message}
+            ]
+        )
+        return completion.chilces[0].message
 
 
