68975409879fb5e5a39082c5488ea869ff1c5e49
yongminkim0501 <yongmingim166@gmail.com>
2025-02-16 16:41:41 +0900

    단어 사이의 유사성 비교 예시 코드 작성

--- context_analysis.py
+++ context_analysis.py
@@ -1,50 +1,8 @@
-'''import urllib3
-import json
-
-def connect_etri(path):
-    with open(path) as f:
-        config = json.load(f)
-        TOKEN = config['etri_api_key']
-    return TOKEN
-
-etri_token = connect_etri("etri_token.json")
-
-openApiURL = "http://aiopen.etri.re.kr:8000/WiseNLU"
-
-accessKey = etri_token
-analysisCode = "wsd"
-
-morp : 형태소 분석
-ner : 개체명 인식
-dparse : 의존 구문 분석
-wsd : 어휘 의미 분석
-
-text = "윤동주(尹東柱, 1917년 12월 30일 ~ 1945년 2월 16일)는 한국의 독립운동가, 시인, 작가이다."+"중국 만저우 지방 지린 성 연변 용정에서 출생하여 명동학교에서 수학하였고, 숭실중학교와 연희전문학교를 졸업하였다. 숭실중학교 때 처음 시를 발표하였고, 1939년 연희전문 2학년 재학 중 소년(少年) 지에 시를 발표하며 정식으로 문단에 데뷔했다."+"일본 유학 후 도시샤 대학 재학 중 , 1943년 항일운동을 했다는 혐의로 일본 경찰에 체포되어 후쿠오카 형무소(福岡刑務所)에 투옥, 100여 편의 시를 남기고 27세의 나이에 옥중에서 요절하였다. 사인이 일본의 생체실험이라는 견해가 있고 그의 사후 일본군에 의한 마루타, 생체실험설이 제기되었으나 불확실하다. 사후에 그의 시집 《하늘과 바람과 별과 시》가 출간되었다."+"일제 강점기 후반의 양심적 지식인으로 인정받았으며, 그의 시는 일제와 조선총독부에 대한 비판과 자아성찰 등을 소재로 하였다. 그의 친구이자 사촌인 송몽규 역시 독립운동에 가담하려다가 체포되어 일제의 생체 실험으로 의문의 죽음을 맞는다. 1990년대 후반 이후 그의 창씨개명 '히라누마'가 알려져 논란이 일기도 했다. 본명 외에 윤동주(尹童柱), 윤주(尹柱)라는 필명도 사용하였다.";
-
-requestJson = {
-    "argument": {
-            "text": text,
-            "analysis_code": analysisCode
-    }
-}
-
-http = urllib3.PoolManager()
-response = http.request(
-    "POST",
-    openApiURL,
-    headers={
-        "Content-Type":"application/json; charset=UTF-8","Authorization":accessKey},
-    body = json.dumps(requestJson)
-)
-
-print("[responseCode] " + str(response.status))
-print("[responBody]")
-print(str(response.data,"utf-8"))
-'''
 import requests
 import json
 from collections import defaultdict
 import json
+import urllib3
 
 def connect_etri(path):
     with open(path) as f:
@@ -152,6 +110,7 @@ try:
         name_entities.sort(key=lambda name_entity: name_entity.count, reverse=True)
     
     # 형태소들 중 명사들에 대해서 많이 노출된 순으로 출력 (최대 5개)
+    '''
     noun_count = 0
     for morpheme in morphemes:
         if morpheme.type in ["NNG", "NNP", "NNB"]:
@@ -160,9 +119,11 @@ try:
             if noun_count >= 5:
                 break
     
-    print("")
+    print("")'''
     
     # 형태소들 중 동사들에 대해서 많이 노출된 순으로 출력 (최대 5개)
+    
+    '''
     verb_count = 0
     for morpheme in morphemes:
         if morpheme.type == "VV":
@@ -172,7 +133,7 @@ try:
                 break
     
     print("")
-    
+    '''
     # 인식된 개체명들 많이 노출된 순으로 출력 (최대 5개)
     for i, name_entity in enumerate(name_entities):
         if i >= 10:
@@ -181,3 +142,35 @@ try:
 
 except Exception as e:
     print(f"예외가 발생했습니다: {str(e)}")
+
+name_characteristic = ["나이","견종","지역","색상"]
+#print(name_entities[3].text, name_entities[3].count)
+#수원시 1 출력
+
+openApiURL_similarity = "http://aiopen.etri.re.kr:8000/WiseWWN/WordRel"
+
+firstWord = "강아지"
+firstSenseId = "1"
+secondWord = "포메라니안"
+secondSenseId = "2"
+
+requestJson = {
+    "argument": {
+        'first_word': firstWord,
+        'first_sense_id': firstSenseId,
+        'second_word': secondWord,
+        'second_sense_id': secondSenseId
+    }
+}
+
+http = urllib3.PoolManager()
+response = http.request(
+    "POST",
+    openApiURL_similarity,
+    headers={"Content-Type": "application/json; charset=UTF-8", "Authorization": etri_token},
+    body=json.dumps(requestJson)
+)
+
+print("[responseCode] " + str(response.status))
+print("[responBody]")
+print(str(response.data,"utf-8"))
